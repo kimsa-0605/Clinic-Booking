32ea69fb7d9a7d548d1ed9c8c2ff6fecae56c09e
kimsa-0605 <holykimsa05@gmail.com>
2024-12-01 21:17:36 +0700

    first commit

---
+++ public/js/inputError.js
@@ -0,0 +1,29 @@
+document.getElementById('submitButton').addEventListener('click', function () {
+    const inputs = document.querySelectorAll(' .form-control'); // Lấy tất cả input trong form
+    let formValid = true;
+    inputs.forEach(input => {
+        if (!input.value.trim()) {
+            formValid = false;
+
+            // Thêm lớp input-error
+            input.classList.add('input-error');
+
+            // Hiển thị popover
+            $(input).popover({
+                trigger: 'manual', // Hiển thị thủ công
+                placement: 'left' // Vị trí popover
+            }).popover('show');
+
+            // Ẩn popover sau 2 giây
+            setTimeout(() => {
+                $(input).popover('hide');
+            }, 2000);
+        } else {
+            // Xóa lỗi nếu đã nhập
+            input.classList.remove('input-error');
+            $(input).popover('hide');
+        }
+    });
+
+
+});
